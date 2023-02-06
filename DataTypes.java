fd8b2cedcc41fd438fea6f3269d1fd63a890e1ee
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-06 21:40:20 +0600

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -1,24 +1,19 @@
 public class DataTypes {
-  public static void main(String[] args) {
-    // data types
-    int $age = 23;
-    float cgpa = 3.45;
-    char letter = 'A';
-    boolean isTrue = false;
-    String str = "Hello World";
-  }
-}
-
-class PrimitiveDataTypes {
-  public static void PDT() {
+  private static void PrimitiveDataTypes() {
     // There are eight primitive data types in Java:
     byte byt = 100;
     short sht = 234;
     int year = 2023;
     long average = 543324;
-    float radius = 2.93;
+    float radius = 2.93f;
     double accuracy = 3.13051;
     boolean isMale = true;
     char chr = 'L';
+    
+    System.out.println(byt);
+  }
+  
+  public static void main(String[] args) {
+    PrimitiveDataTypes();
   }
 }
