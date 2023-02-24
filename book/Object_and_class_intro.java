9a7a616cf17f6017bac87cb64b6b3246081c113a
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-24 23:58:22 +0600

    into fig

---
+++ book/Object_and_class_intro.java
@@ -0,0 +1,9 @@
+package book;
+
+public class Object_and_class_intro {
+    public static void main(String[] args) {
+        Circle myobj = new Circle(4.67);
+
+        System.out.println(myobj.getArea());
+    }
+}
