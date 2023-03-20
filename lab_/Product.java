29d27666c4a0b39978b2eca5ca6228c8fce52af1
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-03-20 15:48:04 +0600

    init

---
+++ lab_/Product.java
@@ -0,0 +1,9 @@
+package lab_;
+
+public class Product {
+    public static void main(String[] args) {
+        System.out.println("id: 1005");
+        System.out.println("title: Java lab");
+        System.out.println("Description: Our first java lab");
+    }
+}
---
+++ lab_/lab20march.java
@@ -0,0 +1,13 @@
+package lab_;
+
+public class lab20march {
+
+    public static void firstHW() {
+        System.out.println("id: 1005");
+        System.out.println("title: Java lab");
+        System.out.println("Description: Our first java lab");
+    }
+    public static void main(String[] args) {
+        firstHW();
+    }
+}
