2f7a1d8519a760b91e764e503b5df452da943f7c
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-05-15 00:42:39 +0600

    Create Student.java
    lab2 `q1` is solved

---
+++ lab_/Student.java
@@ -0,0 +1,74 @@
+/*
+ * Lab 2: May 8, 2023
+ * Question: Write a program to create a class Student with data ‘name, city and age’ along with method printData to display the data. 
+             Create the two objects s1 ,s2 to declare and access the values.
+@Zahangir
+*/
+
+public class Student
+{
+    private String name;
+    private String city;
+    private int age;
+    
+    public void printData() {
+        System.out.println("Name: "+ name);
+        System.out.println("City: "+ city);
+        System.out.println("Age: "+ age);
+    }
+    
+	public static void main(String[] args) {
+	    /* s1 object */
+	    System.out.println("Result of s1 object");
+	    Student s1 = new Student();
+	    s1.name = "POTUS";
+	    s1.city = "Texas";
+	    s1.age = 45;
+	    s1.printData();
+	    
+	    System.out.println("-----\nAccessing values");
+	    System.out.println(s1.name);
+	    System.out.println(s1.city);
+	    System.out.println(s1.age);
+	    
+	    /////////////////////////////
+	    /* s2 object */
+	    System.out.println("\nResult of s1 object");
+	    Student s2 = new Student();
+	    s2.name = "Karoline";
+	    s2.city = "New York";
+	    s2.age = 39;
+	    s2.printData();
+	    
+	    System.out.println("-----\nAccessing values");
+	    System.out.println(s2.name);
+	    System.out.println(s2.city);
+	    System.out.println(s2.age);
+	}
+}
+
+
+/*
+*** Expected output:
+********************
+Result of s1 object
+Name: POTUS
+City: Texas
+Age: 45
+-----
+Accessing values
+POTUS
+Texas
+45
+
+Result of s1 object
+Name: Karoline
+City: New York
+Age: 39
+-----
+Accessing values
+Karoline
+New York
+39
+********************
+*/
