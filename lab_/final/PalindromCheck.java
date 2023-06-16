75af0d01050becfdff0259eaf4684ab24982b6c7
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-06-16 16:25:06 +0600

    Create PalindromCheck.java

---
+++ lab_/final/PalindromCheck.java
@@ -0,0 +1,27 @@
+import java.util.*;
+// string
+public class PalindromCheck
+{
+	public static void main(String[] args) {
+	  Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String userInput = scanner.nextLine();
+		
+		String revString = "";
+		
+		boolean isPrime = true;
+		int strl = userInput.length();
+		int i = strl-1;
+		
+		while(i >= 0) {
+		    revString = revString + userInput.charAt(i);
+		    i--;
+		}
+		
+		if(userInput.toLowerCase().equals(revString.toLowerCase())) {
+		    System.out.println("palindrome");
+    } else {
+		   System.out.println("not palindrome");
+    }
+	}
+}
