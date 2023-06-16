d4be0497606bc511d0423b7f96178a77f3af0fa6
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-06-16 18:34:47 +0600

    Create LCMwithGCD.java

---
+++ lab_/final/LCMwithGCD.java
@@ -0,0 +1,25 @@
+import java.util.*;
+// gcd
+public class LCMwithGCD
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a num1: ");
+		int num1 = scanner.nextInt();
+		
+		System.out.print("Enter a num2: ");
+		int num2 = scanner.nextInt();
+		
+		int gcd = 1;
+		
+		for (int i = 1; i <= num1 && i <= num2; i++) {
+		    if (num1 % i == 0 && num2 % i == 0) {
+		        gcd = i;
+		    }
+		}
+		
+		int LeastCommonMultiple = (num1 * num2) / gcd;
+		System.out.println("LCM = "+ LeastCommonMultiple);
+		
+	}
+}
