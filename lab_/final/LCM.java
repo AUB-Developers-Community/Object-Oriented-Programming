f5aff49c1e4ff6d60927cc4f8942d663ca018c02
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-06-16 18:14:05 +0600

    Create LCM.java

---
+++ lab_/final/LCM.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LCM
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a num1: ");
+		int num1 = scanner.nextInt();
+		
+		System.out.print("Enter a num2: ");
+		int num2 = scanner.nextInt();
+		
+		int LeastCommonMultiple = (num1 > num2) ? num1 : num2;
+		
+		while(true) {
+		    if (LeastCommonMultiple % num1 == 0 && LeastCommonMultiple % num2 == 0) {
+		        System.out.println("LCM = "+ LeastCommonMultiple);
+		        break;
+		    }
+		    LeastCommonMultiple++;
+		}
+		
+	}
+}
