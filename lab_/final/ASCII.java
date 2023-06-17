11effc15379b8752478b771175015a5c7d259dc5
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-06-17 12:56:46 +0600

    Create ASCII.java

---
+++ lab_/final/ASCII.java
@@ -0,0 +1,13 @@
+public class ASCII
+{
+	public static void main(String[] args) {
+	    char ch = 'a';
+	    
+	    int ascii = ch;
+	    
+	    int secodWay = (int) ch;
+	    
+	    System.out.println(ascii);
+	    System.out.println(secodWay);
+	}
+}
